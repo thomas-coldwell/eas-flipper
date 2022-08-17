9aace08d9e235e89ac2ba2a91bb115cfefd09558
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2022-08-17 16:38:40 +0100

    feat: add flipper to EAS

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+//@ts-ignore
+import { connectToDevTools } from "react-devtools-core";
+import { useState } from "react";
+if (__DEV__) {
+  connectToDevTools({
+    host: "localhost",
+    port: 8097,
+  });
+}
 
 export default function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {show && <Text>Open up App.tsx to start working on your app!</Text>}
       <StatusBar style="auto" />
+      <Button title="Press me!" onPress={() => setShow(!show)} />
     </View>
   );
 }
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
